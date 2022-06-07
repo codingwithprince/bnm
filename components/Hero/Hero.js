d6d8884136db2f20dac6c92b8f99b2a14e54d21f
Prince Ahmed <codingwithprince@gmail.com>
2022-06-07 20:03:27 +0600

    added animation

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -3,9 +3,8 @@ import React from 'react'
 import Image from 'next/image'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
-
 import MultiCarousel from './MultiCarousel';
-
+import Typed from "react-typed"
 
 const heroImages =[
   {
@@ -62,17 +61,26 @@ const Hero = () => {
 
   return (
     <div className='pt-[80px] h-[100vh] flex items-center'>
-        <div className="hero md:px-[2%] py-0 grid grid-cols-1 md:grid-cols-2">
-              <div className="hero-text p-2 text-left flex flex-col text-white md:mt-20">
-                  <h2 className='text-8xl font-extrabold font-mono sm:text-[150px] md:mb-5 text-blue-600'>B.N.M</h2>
-                  <p className='text-justify text-gray-400 mb-2 md:w-[80%]'>We specialize in the sheet metal forming process and additional stampings to a desired product quality.</p>
+        <div className="hero md:px-[2%] py-0 grid grid-cols-1 md:grid-cols-3">
+              <div className="hero-text p-2 text-left text-white md:mt-20">
+                  <h2 className='text-8xl font-extrabold font-mono  text-blue-700'> B.N.M </h2>
+                  <Typed
+                  className='text-gray-700 text-2xl my-2'
+                    strings={[
+                          "Avtomobilska Industrija",
+                        ]}
+                        typeSpeed={150}
+                        backSpeed={100}
+                        loop
+              />
+                  <p className='text-justify text-gray-700 my-3 md:w-[80%]'>We specialize in the sheet metal forming process and additional stampings to a desired product quality.</p>
               </div> 
 
 
               {/* =============== Slider with image =================== */}
 
 
-              <div className="hero-image-slider text-gray-700 flex justify-center items-center">
+              <div className="hero-image-slider col-span-2 text-gray-700 flex justify-center items-center">
                 <div className="card w-[100%] bg-white p-1 md:p-5 rounded-lg shadow-lg ">
                     <h3 className='py-5 font-semibold text-2xl'>Our Work</h3>
                     <Carousel showThumbs={true} showArrows={true} showIndicators={false} swipeable={true} infiniteLoop={true} autoPlay={true}>
