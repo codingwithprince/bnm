ac4573a103196dd88d8097c201dd363179a2e23e
Prince Ahmed <codingwithprince@gmail.com>
2022-06-08 11:45:16 +0600

    added typing animation

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 import MultiCarousel from './MultiCarousel';
+import  TypeAnimation  from 'react-type-animation'
 
 
 
@@ -68,14 +69,26 @@ const Hero = () => {
     //   backgroundSize:'cover',
     //   backgroundPosition:'center',
     // }}
-    className='pt-[80px] sm:px-[5%] px-[2px] flex items-center bg-[#EDF2F7]'>
+    className='pt-[80px] sm:px-[5%] px-[2px] sm:py-[80px] flex items-center bg-[#EDF2F7]'>
         <div className="hero md:px-[2%] py-0 grid grid-cols-1 md:grid-cols-3">
               <div className="hero-text p-2 text-left text-white md:mt-20">
                   <h2 className='text-center sm:text-left text-8xl font-extrabold font-mono mb-5 text-blue-700'> B.N.M </h2>
-                  <p className='text-gray-700 md:text-4xl text-2xl'> Avtomobilska Industrija</p>
+                      <TypeAnimation
+                          className='text-gray-700 md:text-4xl md:text-left text-center text-2xl font-semibold '
+                          cursor={true}
+                          sequence={['Avtomobilska Industrija', 4000, '']}
+                          wrapper="h2"
+                          repeat={Infinity}
+                        />
+                
+             
                <p className='text-gray-700 mt-5 text-xl text-justify md:pr-10'>Our field of activity is sheet metal forming, complex stamping and welding for the automotive industry.
 Machine capacities, knowledge and experience give us the opportunity to cooperate also with other industries which require such services or sheet metal products.</p>
-                <button className='mt-5 text-lg font-semibold bg-gray-700 text-white hover:bg-gray-600 px-4 py-2 rounded-full'>Contact</button>
+
+                <div className="buttons text-center sm:text-left">
+                   <button className='mt-5 text-lg font-semibold bg-blue-600 text-white hover:bg-blue-500 px-4 py-2 rounded-full'>Contact</button>
+                </div>
+              
 
                  
               </div> 
@@ -85,7 +98,7 @@ Machine capacities, knowledge and experience give us the opportunity to cooperat
 
 
               <div className="hero-image-slider col-span-2 text-gray-700 flex justify-center items-center">
-                <div className="card w-[100%]  p-1 md:p-5 rounded-lg shadow-lg ">
+                <div className="card w-[100%]  p-1 md:p-5 rounded-lg">
                     <h3 className='py-5 font-semibold text-2xl text-center'>Our Work</h3>
                     <Carousel showThumbs={true} showArrows={true} showIndicators={false} swipeable={true} infiniteLoop={true} autoPlay={true}>
                     {
