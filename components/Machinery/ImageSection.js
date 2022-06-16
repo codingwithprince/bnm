113d2147a913149c8cfe9669bee544dec4b66306
Prince Ahmed <codingwithprince@gmail.com>
2022-06-16 10:37:01 +0600

    added machinery

--- components/Machinery/ImageSection.js
+++ components/Machinery/ImageSection.js
@@ -8,9 +8,9 @@ const ImageSection = ({name, data}) => {
     <h3 className="text-center font-semibold text-2xl py-5 md:py-20 text-blue-800">{name}</h3>
     <div className="img-section gap-5 flex flex-wrap justify-center">
     {
-      data.map(item => <div key={item.id} className='shadow-lg'>
+      data.map(item => <div data-aos="fade-up" key={item.id} className='shadow-lg'>
         <div className="img">
-          <img className='w-[500px] h-[400px] object-cover p-2' src={item.img} alt='' />
+          <img className='w-[400px] h-[300px] hover:scale-105 transition-all ease-in-out  object-cover p-2' src={item.img} alt='' />
         </div>
         <div className="text py-5">
           <h4 className='text-center font-semibold'>{item.title}</h4>
--- components/Machinery/Machinery.js
+++ components/Machinery/Machinery.js
@@ -27,11 +27,53 @@ const cutting =[
   }
 ]
 
+const forming =[
+  {
+    id:1,
+    title:'Line of heavy presses',
+    img:'/images/machinery/forming/velika-stiskalnica-big.jpg',
+    desc1:'',
+    desc2:`Pressure: 5000-16000 kN
+    Table size: 3500 x 2500 mm`
+  },
+  {
+    id:2,
+    title:'Line of middle presses',
+    img:'/images/machinery/forming/srednja-stiskalnica-big.jpg',
+    desc1:'',
+    desc2:`Pressure: 1000-10.000 kN
+    Table size: 1400 x 1500 mm`
+  },
+  {
+    id:3,
+    title:'Line of light presses',
+    img:'/images/machinery/forming/mala-stiskalnica-big.jpg',
+    desc1:'',
+    desc2:`Pressure: 350-1600 kN
+    Table size: 1000 x 700 mm`
+  }
+]
+
+const assembly =[
+  {
+    id:1,
+    title:'Assembly',
+    img:'/images/machinery/assembly/sestavljanje-kabin.jpg',
+    desc1:'',
+    desc2:`Hanging spot welding machines with power of: 40 - 180 kW
+    Stable spot welding machines with power of: 60 - 200 kW`
+  }
+]
+
 const Machinery = () => {
   return (
     <div>
         <h2 className='text-center text-2xl sm:text-4xl font-bold text-gray-700'>Machinery</h2>
         <ImageSection name='Sheet metal cutting' data={cutting} />
+        <ImageSection name='Sheet metal forming' data={forming} />
+        <ImageSection name='Assembly' data={assembly} />
+
+
     </div>
   )
 }
